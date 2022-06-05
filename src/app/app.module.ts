7c891915f73a83f1ba225caeb675e6aec75fb7fa
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-06-05 21:24:31 +0100

    organized exports with barrels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
-import { EventsListComponent } from './events/events-list/events-list.component';
-import { EventThumbnailComponent } from './event/event-thumbnail/event-thumbnail.component';
-import { NavbarComponent } from './nav/navbar/navbar.component';
-import { EventDetailsComponent } from './event-details/event-details/event-details.component';
+
+import {
+  EventComponent,
+  EventDetailsComponent,
+  NavbarComponent,
+  EventThumbnailComponent,
+  EventsListComponent
+} from "./index";
+
 import { appRoutes } from 'src/routes';
 import { RouterModule } from '@angular/router';
-import { EventComponent } from './create/event/event.component';
+
 
 import { ErrorComponent } from './errors/error/error.component';
 
---
+++ src/app/index.ts
@@ -0,0 +1,8 @@
+export * from "./create/event/event.component";
+export * from "./errors/error/error.component";
+export * from "./event/event-thumbnail/event-thumbnail.component";
+export * from "./event-details/event-details/event-details.component";
+export * from "./events/events-list/events-list.component";
+export * from "./nav/navbar/navbar.component";
+
+export * from "./event-route-activator.service";
