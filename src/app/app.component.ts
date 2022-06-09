424d7dc96d99dfd644825795e4e606d780778b0d
Salisu Shuaibu <salisshuaibu11@gmail.com>
2022-06-09 20:39:29 +0100

    Persisit auth state across refreshes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'practice';
+  constructor(private auth: AuthService) {
+
+  }
+
+  ngOnInit() {
+    this.auth.checkAuthenticationStatus();
+  }
 }
